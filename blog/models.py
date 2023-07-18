898485022c2fb53cf2857cada20e4818d09f7700
Quaren Xiong <yaozhou.xiong06@gmail.com>
2023-07-18 15:37:26 -0400

    text editor

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class post(models.Model):
@@ -7,7 +7,7 @@ class post(models.Model):
     date=models.DateTimeField()
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
-    body=models.TextField()
+    body=RichTextField(blank=True,null=True)
     image=models.ImageField(null=True,blank=True,upload_to='blog/pictures/' , default='blog/pictures/default.jpg')
     def __str__(self):
         return f"{self.title} | {self.first_name} {self.last_name}"
--- webpage/settings.py
+++ webpage/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'homepage',
     'videos',
     'blog',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
